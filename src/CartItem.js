3afd98770605b900c18ef7d46952f2a48e5a470e
lakee mangal <lakee.12002794@lpu.in>
2023-01-22 13:52:48 +0530

    add increase and decrease function

--- src/CartItem.js
+++ src/CartItem.js
@@ -4,21 +4,30 @@ class CartItem extends React.Component{
     constructor (){
         super();
         this.state={
-            pricce:999,
-            title:'Mobile Phone',
+            price:6969,
+            title:'Nokia 1200',
             qty:1,
             img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ4qae522N2EzJZbdyGdnALi-BAirqdp8cZc55VoDDtQ&s'
         }
     }
     increaseQuantity=()=>{
-        console.log("hii")
+       // console.log("hii")
+       // setState form 1
+       this.setState({
+        qty:this.state.qty +1
+       })
+    }
+    decreaseQuantity=()=>{
+        this.setState({
+            qty:this.state.qty -1
+        })
     }
     render(){
-        const {price,title,qty}=this.state;
+        const {price,title,qty,img}=this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
-                    <img style={style.image} />
+                    <img src={img} />
                 </div>
                 <div className="right-block">
                     <div style={{fontSize:17}}>{title}</div>
@@ -31,8 +40,15 @@ class CartItem extends React.Component{
                           src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
                           onClick={this.increaseQuantity.bind(this)}
                           />
-                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" />
-                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" />
+                        <img alt="decrease"
+                         className="action-icons"
+                         src="https://cdn-icons-png.flaticon.com/512/992/992683.png" 
+                         onClick={this.decreaseQuantity.bind(this)}
+                         />
+                        <img alt="delete"
+                         className="action-icons"
+                         src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+                         />
                     </div>
                 </div>
             </div>
